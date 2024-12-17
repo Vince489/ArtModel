19ca7819d9260607009b65ad67dc44bd09ab8881
Vince489 <vincentcornelius489@gmail.com>
2024-12-17 14:12:26 -0500

    willReadFrequently

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,19 @@
 const cacheName = 'pixel-art-generator-v20';
 const assetsToCache = [
-    'ArtModel/',             // Root folder
-    'ArtModel/index.html',   // Main file
-    'ArtModel/style.css',    // CSS file
-    'ArtModel/script.js',    // JS file
-    'ArtModel/service-worker.js' // SW itself (optional to cache)
+    '/',             // Root folder
+    '/index.html',   // Main file
+    '/style.css',    // CSS file
+    '/script.js',    // JS file
+    '/service-worker.js' // SW itself (optional to cache)
 ];
+
+// const assetsToCache = [
+//     'ArtModel/',             // Root folder
+//     'ArtModel/index.html',   // Main file
+//     'ArtModel/style.css',    // CSS file
+//     'ArtModel/script.js',    // JS file
+//     'ArtModel/service-worker.js' // SW itself (optional to cache)
+// ];
 
 // Install Event
 self.addEventListener('install', (event) => {
